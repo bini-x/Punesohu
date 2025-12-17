84a1e9d6e72e19d5a1828cd706228e00a0cc7d46
bini-x <erblinhiseni@gmail.com>
2025-12-18 00:42:07 +0100

    'profili' of employer now shows the job postings of that employer

--- frontend/src/components/Profili.jsx
+++ frontend/src/components/Profili.jsx
@@ -3,10 +3,10 @@ import "../index.css";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import Header from "./Header";
-import ShpalljaCard from "./ShpalljaCard";
 
 function Profili() {
-  const [perdoruesiData, setPerdoruesiData] = useState(null);
+  const [perdoruesiData, setPerdoruesiData] = useState({});
+  const [shpalljaData, setShpalljaData] = useState([]);
   const { id } = useParams();
 
   useEffect(() => {
@@ -16,6 +16,7 @@ function Profili() {
           `http://localhost:3000/api/profili/${id}`,
         );
         setPerdoruesiData(response.data.data);
+        // console.log(perdoruesiData);
       } catch (error) {
         console.error(error);
       }
@@ -25,6 +26,37 @@ function Profili() {
       fetchData();
     }
   }, [id]);
+
+  useEffect(() => {
+    console.log("perdoruesi: ", perdoruesiData);
+  }, [perdoruesiData]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:3000/api/shpallja/kompania",
+        );
+        if (Array.isArray(response.data.data)) {
+          const shpalljetFiltruara = response.data.data.filter((shpallja) => {
+            return shpallja.emailKompanise === perdoruesiData.email;
+          });
+
+          if (shpalljetFiltruara.length > 0) {
+            setShpalljaData(shpalljetFiltruara);
+          }
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, [perdoruesiData]);
+
+  useEffect(() => {
+    console.log("shpallja: ", shpalljaData);
+  }, [shpalljaData]);
 
   if (!perdoruesiData) {
     return (
@@ -43,6 +75,18 @@ function Profili() {
       <h1>{perdoruesiData.emri || perdoruesiData.kompania}</h1>
       <h2>{perdoruesiData.mbiemri}</h2>
       <p>{perdoruesiData.email}</p>
+      {shpalljaData.map((sh) => {
+        return (
+          <div className="border m-2 p-2">
+            <h3>Shpallja e Punes:</h3>
+            <div className="border m-2 p-2">
+              <p>Pozita e Punes: {sh.pozitaPunes}</p>
+              <p>Lokacioni: {sh.lokacioniPunes}</p>
+              <p>Niveli: {sh.niveliPunes}</p>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 }
