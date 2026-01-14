4176ce8b85c8c9cb9e9a85011724437191767051
ArtBugaqku <artbugaqku@gmail.com>
2026-01-14 12:03:13 +0100

    test

---
+++ backend/models/aplikimiSchema.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const aplikimiSchema = new mongoose.Schema({
+  shpalljaId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Shpallja",
+    required: true,
+  },
+  emailAplikantit: {
+    type: String,
+    required: true,
+  },
+  emriAplikantit: {
+    type: String,
+    required: true,
+  },
+  mbiemriAplikantit: {
+    type: String,
+    required: true,
+  },
+  eksperienca: {
+    type: String,
+    required: false,
+  },
+  niveliPunes: {
+    type: String,
+    required: false,
+  },
+  nrTelefonit: {
+    type: Number,
+    required: false,
+  },
+  letraMotivuese: {
+    type: String,
+    required: true,
+  },
+});
+
+const Aplikimi = mongoose.model("Aplikimi", aplikimiSchema, "aplikimet");
+module.exports = Aplikimi;
